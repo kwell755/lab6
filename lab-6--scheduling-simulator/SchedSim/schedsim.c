349d3f29aedf56b2babc8bd879c6d553db25613f
kwell755 <49081829+kwell755@users.noreply.github.com>
2020-12-08 22:37:12 -0500

    Add files via upload

---
+++ lab-6--scheduling-simulator/SchedSim/schedsim.c
@@ -0,0 +1,246 @@
+// C program for implementation of Simulation 
+#include<stdio.h> 
+#include<limits.h>
+#include<stdlib.h>
+#include "process.h"
+#include "util.h"
+
+// Function to find the waiting time for all  
+// processes
+void findWaitingTimeRR(ProcessType plist[], int n,int quantum) 
+{ 
+    int arr[n];
+    int x;
+    int time = 0; 
+
+    for (x = 0; x < n; x++){
+      arr[x] = plist[x].bt;
+      plist[x].wt = 0; 
+    }
+  
+    int done = 0; 
+    while(!done){
+      done = 1; 
+      for(x = 0; x < n; x++){
+        if (arr[x] > 0){
+          done = 0; 
+
+          if (arr[x] > quantum){
+            time = time + quantum;
+            arr[x] -= quantum;
+          }
+         
+          else{
+            time = time + arr[x];
+            plist[x].wt = time - plist[x].bt;
+            arr[x] = 0;   
+          }
+        }
+
+      }
+    }
+} 
+
+// Function to find the waiting time for all  
+// processes 
+void findWaitingTimeSJF(ProcessType plist[], int n)
+{ 
+     
+      int x;
+      int processCount = 0;
+      int currTime = 0;
+      int finTime = 0; 
+      int arr[n];
+
+      for(x = 0; x < n; x++){
+        arr[x] = plist[x].bt;
+      }
+
+      while(processCount < n){
+        int min_index = 0;
+        for(x = 1; x < n; x++){
+          if (arr[x] < arr[min_index]){
+            min_index = x;
+          }
+        }
+        
+        arr[min_index] -= 1;
+        
+        if (arr[min_index] == 0){
+          processCount += 1;
+          finTime = currTime + 1;          
+          plist[min_index].wt = finTime - plist[min_index].bt;
+          arr[min_index] = 9999;
+        }
+      currTime += 1;
+      }
+} 
+
+// Function to find the waiting time for all  
+// processes 
+void findWaitingTime(ProcessType plist[], int n)
+{ 
+    // waiting time for first process is 0, or the arrival time if not 
+    plist[0].wt = 0 +  plist[0].art; 
+  
+    // calculating waiting time 
+    for (int  i = 1; i < n ; i++ ) 
+        plist[i].wt =  plist[i-1].bt + plist[i-1].wt; 
+} 
+  
+// Function to calculate turn around time 
+void findTurnAroundTime( ProcessType plist[], int n)
+{ 
+    // calculating turnaround time by adding bt[i] + wt[i] 
+    for (int  i = 0; i < n ; i++) 
+        plist[i].tat = plist[i].bt + plist[i].wt; 
+} 
+  
+// Function to sort the Process acc. to priority
+int my_comparer(const void *this, const void *that)
+{ 
+  
+    if (((ProcessType*)this)->pri < ((ProcessType*)that)->pri){
+        return 1;
+      }
+    return 0;
+} 
+
+//Function to calculate average time 
+void findavgTimeFCFS( ProcessType plist[], int n) 
+{ 
+    //Function to find waiting time of all processes 
+    findWaitingTime(plist, n); 
+  
+    //Function to find turn around time for all processes 
+    findTurnAroundTime(plist, n); 
+  
+    //Display processes along with all details 
+    printf("\n*********\nFCFS\n");
+}
+
+//Function to calculate average time 
+void findavgTimeSJF( ProcessType plist[], int n) 
+{ 
+    //Function to find waiting time of all processes 
+    findWaitingTimeSJF(plist, n); 
+  
+    //Function to find turn around time for all processes 
+    findTurnAroundTime(plist, n); 
+  
+    //Display processes along with all details 
+    printf("\n*********\nSJF\n");
+}
+
+//Function to calculate average time 
+void findavgTimeRR( ProcessType plist[], int n, int quantum) 
+{ 
+    //Function to find waiting time of all processes 
+    findWaitingTimeRR(plist, n, quantum); 
+  
+    //Function to find turn around time for all processes 
+    findTurnAroundTime(plist, n); 
+  
+    //Display processes along with all details 
+    printf("\n*********\nRR Quantum = %d\n", quantum);
+}
+
+//Function to calculate average time 
+void findavgTimePriority( ProcessType plist[], int n) 
+{ 
+  
+   
+  
+    qsort(plist, n, sizeof(ProcessType), my_comparer);
+    findWaitingTime(plist, n); 
+    findTurnAroundTime(plist, n);   
+    printf("\n*********\nPriority\n");
+}
+
+void printMetrics(ProcessType plist[], int n)
+{
+    int total_wt = 0, total_tat = 0; 
+    float awt, att;
+    
+    printf("\tProcesses\tBurst time\tWaiting time\tTurn around time\n"); 
+  
+    // Calculate total waiting time and total turn  
+    // around time 
+    for (int  i=0; i<n; i++) 
+    { 
+        total_wt = total_wt + plist[i].wt; 
+        total_tat = total_tat + plist[i].tat; 
+        printf("\t%d\t\t%d\t\t%d\t\t%d\n", plist[i].pid, plist[i].bt, plist[i].wt, plist[i].tat); 
+    } 
+  
+    awt = ((float)total_wt / (float)n);
+    att = ((float)total_tat / (float)n);
+    
+    printf("\nAverage waiting time = %.2f", awt); 
+    printf("\nAverage turn around time = %.2f\n", att); 
+} 
+
+ProcessType * initProc(char *filename, int *n) 
+{
+  	FILE *input_file = fopen(filename, "r");
+	  if (!input_file) {
+		    fprintf(stderr, "Error: Invalid filepath\n");
+		    fflush(stdout);
+		    exit(0);
+	  }
+
+    ProcessType *plist = parse_file(input_file, n);
+  
+    fclose(input_file);
+  
+    return plist;
+}
+  
+// Driver code 
+int main(int argc, char *argv[]) 
+{ 
+    int n; 
+    int quantum = 2;
+
+    ProcessType *proc_list;
+  
+    if (argc < 2) {
+		   fprintf(stderr, "Usage: ./schedsim <input-file-path>\n");
+		   fflush(stdout);
+		   return 1;
+	   }
+    
+  // FCFS
+    n = 0;
+    proc_list = initProc(argv[1], &n);
+  
+    findavgTimeFCFS(proc_list, n);
+    
+    printMetrics(proc_list, n);
+  
+  // SJF
+    n = 0;
+    proc_list = initProc(argv[1], &n);
+   
+    findavgTimeSJF(proc_list, n); 
+   
+    printMetrics(proc_list, n);
+  
+  // Priority
+    n = 0; 
+    proc_list = initProc(argv[1], &n);
+    
+    findavgTimePriority(proc_list, n); 
+    
+    printMetrics(proc_list, n);
+    
+  // RR
+    n = 0;
+    proc_list = initProc(argv[1], &n);
+    
+    findavgTimeRR(proc_list, n, quantum); 
+    
+    printMetrics(proc_list, n);
+    
+    return 0; 
+} 
